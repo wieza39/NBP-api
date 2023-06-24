82101829c7d06d0a23efc0ba471c46a97e2524e4
s24279 <jakubwiecek93@gmail.com>
2023-06-24 12:44:33 +0200

    initial commit

---
+++ s24279/src/main/java/jaz/s24279/nbp/JazS24279NbpApplication.java
@@ -0,0 +1,13 @@
+package jaz.s24279.nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS24279NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS24279NbpApplication.class, args);
+	}
+
+}
---
+++ s24279/src/test/java/jaz/s24279/nbp/JazS24279NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package jaz.s24279.nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS24279NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
